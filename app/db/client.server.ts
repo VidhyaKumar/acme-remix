f1365cf0fe18a0a27e741cd91985a5ebdc23e55f
Vidhya Kumar <vidhya.kumar@outlook.com>
2023-12-05 21:20:43 +0800

    feat: update database configuration

--- app/db/client.server.ts
+++ app/db/client.server.ts
@@ -4,8 +4,8 @@ import { drizzle } from "drizzle-orm/libsql"
 import * as schema from "./schema.server"
 
 const client = createClient({
-  url: process.env.DATABASE_URL!,
-  authToken: process.env.DATABASE_TOKEN!,
+  url: process.env.TURSO_DATABASE_URL!,
+  authToken: process.env.TURSO_AUTH_TOKEN!,
 })
 
 const db = drizzle(client, {
